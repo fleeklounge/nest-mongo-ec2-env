e8383e661e186dcab96cb1e2aecfbb7accd0ee7d
Asanobm <38549104+asanobm@users.noreply.github.com>
2022-04-10 10:39:52 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MongooseModule} from "@nestjs/mongoose";
+import {DATASOURCE} from "./env";
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(DATASOURCE)],
   controllers: [AppController],
   providers: [AppService],
 })
